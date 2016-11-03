8693b0c236b72642ef300767f7927080db7c738c
SunnyChan <edward1879314@gmail.com>
2016-11-03 14:46:01 +0800

    step1: Created a ListView

--- app/src/main/java/com/example/android/weatherapppickup/MainActivity.java
+++ app/src/main/java/com/example/android/weatherapppickup/MainActivity.java
@@ -2,15 +2,22 @@ package com.example.android.weatherapppickup;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
 
+    /** declare a ListView object named listView */
+    private ListView listView;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        /** bind the ListView in the layout with the listView object here by findViewById, then cast the view to a ListView */
+        listView = (ListView) findViewById(R.id.lv_main);
+
 
 
     }
