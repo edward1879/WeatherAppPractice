9b7b179549251f888443ebe57954f302a324eb97
SunnyChan <edward1879314@gmail.com>
2016-11-04 04:54:41 +0800

    step:5 finished the layout for the listView

--- app/src/main/java/com/example/android/weatherapppickup/MainActivity.java
+++ app/src/main/java/com/example/android/weatherapppickup/MainActivity.java
@@ -4,10 +4,14 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.ListView;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
 
     /** declare a ListView object named listView */
     private ListView listView;
+    private WeatherListAdapter weatherListAdapter;
+    private ArrayList<Weather> weatherArrayList;
 
 
     @Override
@@ -17,6 +21,12 @@ public class MainActivity extends AppCompatActivity {
 
         /** bind the ListView in the layout with the listView object here by findViewById, then cast the view to a ListView */
         listView = (ListView) findViewById(R.id.lv_main);
+
+        weatherArrayList = Weather.sampleWeatherArrayList(20);
+        weatherListAdapter = new WeatherListAdapter(weatherArrayList,this);
+        listView.setAdapter(weatherListAdapter);
+
+
 
     }
 }
--- app/src/main/java/com/example/android/weatherapppickup/WeatherListAdapter.java
+++ app/src/main/java/com/example/android/weatherapppickup/WeatherListAdapter.java
@@ -24,13 +24,13 @@ public class WeatherListAdapter extends BaseAdapter{
         this.context = context;
     }
 
-    /** return the number of cell to use */
+    /** return the number of item in the arraylist */
     @Override
     public int getCount() {
         return dataset.size();
     }
 
-
+    /** get the object with index i from the arraylist of weather */
     @Override
     public Object getItem(int i) {
         return dataset.get(i);
@@ -41,6 +41,13 @@ public class WeatherListAdapter extends BaseAdapter{
         return 0;
     }
 
+    /** return the layout of a cell
+     * use a ViewHolder to hold the views to increase the performance of the listView
+     * LayoutInflater: inflate the cell layout to the view object
+     * findViewById from the view object and bind them to the corresponding views in the ViewHolder
+     *
+     * then set text and image to the corresponding views
+     * */
     @Override
     public View getView(int i, View view, ViewGroup viewGroup) {
         Weather weather = (Weather) getItem(i);
@@ -57,7 +64,8 @@ public class WeatherListAdapter extends BaseAdapter{
             viewHolder = (ViewHolder) view.getTag();
         }
 
-        viewHolder.ivWeatherImage.setImageDrawable(context.getApplicationContext().getResources().getDrawable(weather.getWeatherIcon()));
+        viewHolder.ivWeatherImage.setImageDrawable(
+                context.getApplicationContext().getResources().getDrawable(weather.getWeatherIcon()));
         viewHolder.tvWeatherDescription.setText(weather.getWeatherDescription());
         viewHolder.tvAverageTemp.setText(weather.getAverageTemp());
         viewHolder.tvDate.setText(weather.getDate());
@@ -65,6 +73,7 @@ public class WeatherListAdapter extends BaseAdapter{
         return view;
     }
 
+    /** an inner class to hold views */
     private class ViewHolder{
         ImageView ivWeatherImage;
         TextView tvDate;
